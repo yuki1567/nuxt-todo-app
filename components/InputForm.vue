3d44deb1be155f4b659401255d2847ee1a80dd9a
yuki1567 <19961017yuki@gmail.com>
2025-02-22 14:02:28 +0900

    add: タスク入力欄の作成

---
+++ components/InputForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="input-form">
+    <form @submit="">
+      <input type="text" v-model="inputText" @change="handleChange" />
+      <button className="add-button">
+        <span className="material-icons icon">add</span>
+      </button>
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+const inputText = ref("");
+</script>
+
+<style>
+.input-form {
+  margin: 0 auto;
+  display: flex;
+  align-items: stretch;
+  justify-content: center;
+}
+
+.input-form input {
+  padding: 3px 8px;
+  border: 1px solid rgb(218 220 222);
+  border-radius: 3px;
+  flex-grow: 1;
+}
+
+.add-button {
+  background: rgb(83, 164, 195);
+}
+</style>
